5c2b22de90745ee8ef59221198d3d3304b11a3bc
xotlr333 <xotlr333@naver.com>
2023-08-31 09:56:32 +0900

    백엔드 세팅

---
+++ backend/src/main/java/com/easypeach/shroopadmin/ShroopadminApplication.java
@@ -0,0 +1,13 @@
+package com.easypeach.shroopadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShroopadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShroopadminApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/easypeach/shroopadmin/ShroopadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easypeach.shroopadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShroopadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
