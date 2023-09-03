aa05838458116e1ae9684de285cf96bd3a547e57
박지윤 <jyun1937@naver.com>
2023-09-03 19:45:39 +0900

    Design: 공통 타이틀 구현

---
+++ frontend/src/components/CommonTitle.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script setup>
+defineProps({
+  title: String,
+});
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+  margin: 50px 0;
+  font-size: 25px;
+}
+</style>
