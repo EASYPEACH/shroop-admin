df76512064ec433812c8c5f37cd89bdc43cdc2b3
xotlr333 <xotlr333@naver.com>
2023-09-06 15:30:57 +0900

    Refactor: 카테고리명을 유니크로 변경

--- backend/src/main/java/com/easypeach/shroopadmin/modules/global/exception/ExceptionControllerAdvice.java
+++ backend/src/main/java/com/easypeach/shroopadmin/modules/global/exception/ExceptionControllerAdvice.java
@@ -1,8 +1,33 @@
 package com.easypeach.shroopadmin.modules.global.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.easypeach.shroopadmin.modules.global.exception.dto.ErrorResponse;
+import com.easypeach.shroopadmin.modules.product.exception.CategoryDeleteException;
+import com.easypeach.shroopadmin.modules.product.exception.DuplicateCategoryException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
+	@ExceptionHandler({
+		CategoryDeleteException.class,
+		DuplicateCategoryException.class
+	})
+	public ResponseEntity<ErrorResponse> handleNotExistException(final RuntimeException e) {
+		String errorMessage = e.getMessage();
+		ErrorResponse errorResponse = new ErrorResponse(errorMessage);
+		return ResponseEntity.status(400).body(errorResponse);
+	}
 
+	@ExceptionHandler
+	public ResponseEntity<ErrorResponse> handleException(final Exception e) {
+
+		log.error("ERROR: ", e);
+		ErrorResponse errorResponse = new ErrorResponse("내부 서버에 문제가 발생하여 확인 중 입니다");
+		return ResponseEntity.internalServerError().body(errorResponse);
+	}
 }
--- backend/src/main/java/com/easypeach/shroopadmin/modules/product/domain/Category.java
+++ backend/src/main/java/com/easypeach/shroopadmin/modules/product/domain/Category.java
@@ -23,7 +23,7 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(length = 50, nullable = false)
+	@Column(length = 50, nullable = false, unique = true)
 	private String name;
 
 	public static Category createCategory(final String name) {
