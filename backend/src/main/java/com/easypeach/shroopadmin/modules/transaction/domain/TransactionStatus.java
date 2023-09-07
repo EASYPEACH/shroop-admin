e2bd767134d141b881e2ab4587069fd98964eedc
박지윤 <jyun1937@naver.com>
2023-09-07 14:40:21 +0900

    Feat: 거래상태 이넘 추가

--- backend/src/main/java/com/easypeach/shroopadmin/modules/transaction/domain/TransactionStatus.java
+++ backend/src/main/java/com/easypeach/shroopadmin/modules/transaction/domain/TransactionStatus.java
@@ -1,5 +1,20 @@
 package com.easypeach.shroopadmin.modules.transaction.domain;
 
 public enum TransactionStatus {
-	PURCHASE_REQUEST, PURCHASE_CONFIRM, RETURN_REQUEST, RETURN_COMPLETE, COMPLETE
+
+	PURCHASE_REQUEST("구매신청"), 
+	PURCHASE_CONFIRM("거래완료"), 
+	RETURN_REQUEST("반품신청"), 
+	RETURN_COMPLETE("반품완료"), 
+	COMPLETE("배송완료");
+
+	private final String name;
+
+	TransactionStatus(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
