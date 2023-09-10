049a8d470d04e5c05ce39e3e6793832222ea0968
jyoung7988 <jyoung7988@naver.com>
2023-09-10 15:46:02 +0900

    Feat: 신고,중재 상세 페이지에서 회원을 차단할 수 있도록 추가한다

--- frontend/src/views/report/ReportDetailsView.vue
+++ frontend/src/views/report/ReportDetailsView.vue
@@ -23,6 +23,30 @@
             () => $router.push(`/user/${reportDetails.reporterId}`)
           "
         />
+    
+      </li>
+      <li>
+        <h2>피신고자 UID</h2>
+        <manage-button
+          :button-text="reportDetails.reporteeId.toString()"
+          :handle-click="
+            () => $router.push(`/user/${reportDetails.reporteeId}`)
+          "
+        />
+        <manage-button
+          v-if="reportDetails.reporteeStatus === 'ROLE_USER'"
+          :button-text="'계정 차단'"
+          :handle-click="
+            () => handleBlockUser(reportDetails.reporteeId)
+          "
+        />
+        <manage-button
+          v-if="reportDetails.reporteeStatus === 'ROLE_NOT_AUTH_USER'"
+          :button-text="'차단 해제'"
+          :handle-click="
+            () => handleBlockUser(reportDetails.reporteeId)
+          "
+        />
       </li>
       <li>
         <h2>상품 PID</h2>
@@ -74,6 +98,7 @@ const reportDetails = ref({
   reporterId: 0,
   sellerId: 0,
   reportReason: "",
+  reporteeId: 0,
 });
 const imgDialog = ref(false);
 const imgSrc = ref("");
@@ -99,6 +124,41 @@ const handleUpdateReportStatus = () => {
 };
 
 onBeforeMount(async () => {
+  handleGetReportInfo();
+});
+
+
+const handleBlockUser = async (id) => {
+  if (confirm("차단을 진행하겠습니까?")) {
+    try {
+      const res = await patchApi({
+        url: `/api/members/${id}/role`,
+      });
+      
+      alert(res.message);
+      handleGetReportInfo();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+const handleUnBlockUser = async (id) => {
+  if (confirm("차단을 해제하시겠습니까")) {
+    try {
+      const res = await patchApi({
+        url: `/api/members/${id}/role`,
+      });
+      
+      alert(res.message);
+      handleGetReportInfo();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+};
+
+const handleGetReportInfo = async () =>{
   try {
     const response = await getApi({
       url: `/api/reports/${route.params.id}`,
@@ -109,11 +169,14 @@ onBeforeMount(async () => {
     reportDetails.value.reportReason = response.content;
     reportDetails.value.productId = response.productId;
     reportDetails.value.sellerId = response.sellerId;
+     reportDetails.value.reporteeId = response.reporteeId;
+     reportDetails.value.reporteeStatus = response.reporteeStatus;
     isConfirm.value = response.status === "REPORT_REQUEST" ? false : true;
   } catch (error) {
     console.error(error);
   }
-});
+}
+
 </script>
 
 <style lang="scss" scoped>
