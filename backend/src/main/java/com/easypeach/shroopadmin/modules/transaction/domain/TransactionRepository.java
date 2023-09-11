2423d3962e656315249115fc7c7cb12c4b295bb5
kongtaesic <xotlr333@naver.com>
2023-09-11 19:41:06 +0900

    Feat: 거래 전체 조회 API 구현

--- backend/src/main/java/com/easypeach/shroopadmin/modules/transaction/domain/TransactionRepository.java
+++ backend/src/main/java/com/easypeach/shroopadmin/modules/transaction/domain/TransactionRepository.java
@@ -1,9 +1,25 @@
 package com.easypeach.shroopadmin.modules.transaction.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.easypeach.shroopadmin.modules.product.domain.Product;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+	@Query(value = "select t from Transaction t "
+		+ "join fetch t.buyer b "
+		+ "join fetch t.seller s "
+		+ "join fetch t.product p "
+		+ "where b.loginId like %:searchWord% or s.loginId like %:searchWord% or p.title like %:searchWord% "
+		, countQuery = "select count(t) from Transaction t "
+		+ "join t.buyer b "
+		+ "join t.seller s "
+		+ "join t.product p "
+		+ "where b.loginId like %:searchWord% or s.loginId like %:searchWord% or p.title like %:searchWord% ")
+	Page<Transaction> findAllFetchJoin(String searchWord, Pageable pageable);
+
 	Transaction findByProduct(Product product);
 }
